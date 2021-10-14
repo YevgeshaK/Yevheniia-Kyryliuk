460a61a8f12826359e3d33f71191ad0e9dc6a057
Andrey Nazarenko <Limelaudio@gmail.com>
2021-10-14 20:40:33 +0300

    add mobile version and minify files

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-const template = {
-  carousel() {
-    $('.owl-carousel').owlCarousel({
-      loop: false,
-      margin: 20,
-      nav: false,
-      items: 3,
-    });
-  },
-};
-
-template.carousel();
+const template={carousel(){$(".owl-carousel").owlCarousel({loop:!1,margin:20,nav:!1,items:3,responsive:{0:{items:1,margin:20},768:{items:2,margin:20},1024:{items:3,margin:20}}})},mobileMenu(){$(".header").on("click",".js-menu-toggle",function(){$(this).toggleClass("active"),$(this).siblings(".header-menu").toggleClass("active")})},smoothScrool(){document.querySelectorAll('a[href^="#"]').forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"}),$(".header-menu").removeClass("active"),$(".js-menu-toggle").removeClass("active")})})}};template.carousel(),template.mobileMenu(),template.smoothScrool();
